22d68cf27d0a315873e57beffc6b09f49fbc35cd
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-11-29 08:42:02 -0300

    cmd: initial api (main)
    cmd: initial api (main)

---
+++ cmd/api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/rafaelsanzio/go-core/pkg/api"
+	"github.com/rafaelsanzio/go-core/pkg/applog"
+	"github.com/rafaelsanzio/go-core/pkg/errs"
+	"github.com/rafaelsanzio/go-core/pkg/store"
+)
+
+func main() {
+	log.Println("starting up...")
+	store.GetStore()
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		_ = errs.ErrMongoConnect.Throwf(applog.Log, errs.ErrFmt, err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		_ = errs.ErrMongoConnect.Throwf(applog.Log, errs.ErrFmt, err)
+	}
+
+	defer client.Disconnect(ctx)
+
+	log.Println("MongoDB server is healthy.")
+	log.Fatal(http.ListenAndServe(":8000", api.NewRouter()))
+}
