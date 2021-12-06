1d8fe82b152a97939337637482b14031f2157273
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-12-06 08:14:31 -0300

    pkg: user
    pkg: user

---
+++ pkg/user/user.go
@@ -0,0 +1,26 @@
+package user
+
+import (
+	"context"
+
+	"github.com/rafaelsanzio/go-core/pkg/errs"
+)
+
+type UserRepo interface {
+	Insert(ctx context.Context, u User) errs.AppError
+	GetUser(ctx context.Context, id string) (User, errs.AppError)
+}
+
+type User struct {
+	ID   string `bson:"_id"`
+	Name string
+	Age  int
+}
+
+func (u User) GetID() string {
+	return u.ID
+}
+
+func (u *User) SetID(id string) {
+	u.ID = id
+}
