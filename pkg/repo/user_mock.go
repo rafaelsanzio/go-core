e08e97ab0ed56318e07d9615b3230d52dd611c7f
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-12-13 10:02:37 -0300

    pkg: repo user mock
    pkg: repo user mock

---
+++ pkg/repo/user_mock.go
@@ -0,0 +1,30 @@
+package repo
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rafaelsanzio/go-core/pkg/errs"
+	"github.com/rafaelsanzio/go-core/pkg/user"
+)
+
+type MockUserRepo struct {
+	user.UserRepo
+	InsertFunc  func(ctx context.Context, u user.User) errs.AppError
+	GetUserFunc func(ctx context.Context, id string) (user.User, errs.AppError)
+}
+
+func (m MockUserRepo) Insert(ctx context.Context, u user.User) errs.AppError {
+	if m.InsertFunc != nil {
+		return m.InsertFunc(ctx, u)
+	}
+	fmt.Println("InsertFunc is nil", u)
+	return m.UserRepo.Insert(ctx, u)
+}
+
+func (m MockUserRepo) GetUser(ctx context.Context, id string) (user.User, errs.AppError) {
+	if m.GetUserFunc != nil {
+		return m.GetUserFunc(ctx, id)
+	}
+	return m.UserRepo.GetUser(ctx, id)
+}
