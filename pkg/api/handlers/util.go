f4c0a5ab124979550d3382aea9ada4c745e6e669
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-12-15 16:03:03 -0300

    pkg: api utils
    pkg: api utils

---
+++ pkg/api/handlers/util.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/rafaelsanzio/go-core/pkg/applog"
+	"github.com/rafaelsanzio/go-core/pkg/errs"
+)
+
+func httpUnprocessableEntity(w http.ResponseWriter, message string) {
+	w.WriteHeader(http.StatusUnprocessableEntity)
+	_, err := w.Write([]byte(message))
+	if err != nil {
+		_ = errs.ErrResponseWriter.Throwf(applog.Log, "error writing response body, err: [%v]", err)
+	}
+}
+
+func httpInternalServerError(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
