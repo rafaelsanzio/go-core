9fc656a84b7912739e308b888d64f8aa0aabf43b
Rafael Sanzio <rafaelsanzio27@gmail.com>
2021-12-21 08:56:11 -0300

    pkg: handle api get user
    pkg: handle api get user

---
+++ pkg/api/handlers/handle_get_user.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rafaelsanzio/go-core/pkg/applog"
+	"github.com/rafaelsanzio/go-core/pkg/errs"
+	"github.com/rafaelsanzio/go-core/pkg/repo"
+)
+
+func HandleGetUser(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if id == "" {
+		_ = errs.ErrGettingParam.Throwf(applog.Log, "error getting param, err: [%v]", id)
+		httpNotFound(w)
+		return
+	}
+
+	user, err := repo.GetUserRepo().GetUser(ctx, id)
+	if err != nil {
+		httpInternalServerError(w)
+		return
+	}
+
+	data, err_ := json.Marshal(user)
+	if err_ != nil {
+		_ = errs.ErrMarshalingJson.Throwf(applog.Log, errs.ErrFmt, err_)
+		httpInternalServerError(w)
+		return
+	}
+
+	_, err_ = w.Write(data)
+	if err_ != nil {
+		_ = errs.ErrResponseWriter.Throwf(applog.Log, "error writing timeline to response, err: [%v]", err_)
+		httpInternalServerError(w)
+		return
+	}
+
+	w.WriteHeader(200)
+}
