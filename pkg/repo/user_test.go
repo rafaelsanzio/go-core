f1fab80ad495926638765baadc4b8901f13ff70e
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-01-19 14:50:16 -0300

    test: implement update to user
    test: implement update to user

--- pkg/repo/user_test.go
+++ pkg/repo/user_test.go
@@ -4,52 +4,83 @@ import (
 	"context"
 	"testing"
 
+	"github.com/rafaelsanzio/go-core/pkg/errs"
 	"github.com/rafaelsanzio/go-core/pkg/model"
-	"github.com/rafaelsanzio/go-core/pkg/store"
+	"github.com/rafaelsanzio/go-core/pkg/user"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepoInsert(t *testing.T) {
 	ctx := context.Background()
+
+	SetUserRepo(MockUserRepo{
+		InsertFunc: func(ctx context.Context, u user.User) errs.AppError {
+			return nil
+		},
+	})
+	defer SetUserRepo(nil)
+
 	newUser := model.PrototypeUser()
 
 	err := GetUserRepo().Insert(ctx, newUser)
 	assert.NoError(t, err)
-	_ = store.GetStore().DeleteOne(ctx, UserCollection, newUser.ID)
 }
 
 func TestUserRepoGet(t *testing.T) {
 	ctx := context.Background()
+
+	SetUserRepo(MockUserRepo{
+		GetFunc: func(ctx context.Context, id string) (*user.User, errs.AppError) {
+			user := model.PrototypeUser()
+			return &user, nil
+		},
+	})
+	defer SetUserRepo(nil)
+
 	newUser := model.PrototypeUser()
 
-	err := GetUserRepo().Insert(ctx, newUser)
+	user, err := GetUserRepo().Get(ctx, "new-user-id")
 	assert.NoError(t, err)
 
-	user, err := GetUserRepo().Get(ctx, newUser.ID)
-	assert.NoError(t, err)
-
-	assert.Equal(t, newUser.ID, user.ID)
 	assert.Equal(t, newUser.Name, user.Name)
 	assert.Equal(t, newUser.Age, user.Age)
-
-	_ = store.GetStore().DeleteOne(ctx, UserCollection, newUser.ID)
 }
 
 func TestUserRepoList(t *testing.T) {
 	ctx := context.Background()
-	newUser := model.PrototypeUser()
 
-	err := GetUserRepo().Insert(ctx, newUser)
-	assert.NoError(t, err)
-	err = GetUserRepo().Insert(ctx, newUser)
-	assert.NoError(t, err)
+	SetUserRepo(MockUserRepo{
+		ListFunc: func(ctx context.Context) ([]user.User, errs.AppError) {
+			userMock := model.PrototypeUser()
+			userMock2 := model.PrototypeUser()
+
+			return []user.User{userMock, userMock2}, nil
+		},
+	})
+	defer SetUserRepo(nil)
 
 	users, err := GetUserRepo().List(ctx)
 	assert.NoError(t, err)
 
 	assert.Equal(t, 2, len(users))
+}
 
-	_ = store.GetStore().DeleteOne(ctx, UserCollection, users[0].ID)
-	_ = store.GetStore().DeleteOne(ctx, UserCollection, users[1].ID)
+func TestUserRepoUpdate(t *testing.T) {
+	ctx := context.Background()
+
+	SetUserRepo(MockUserRepo{
+		UpdateFunc: func(ctx context.Context, u user.User) (*user.User, errs.AppError) {
+			return &u, nil
+		},
+	})
+	defer SetUserRepo(nil)
+
+	newUser := model.PrototypeUser()
+
+	userUpdated, err := GetUserRepo().Update(ctx, newUser)
+	assert.NoError(t, err)
+
+	assert.Equal(t, newUser.Name, userUpdated.Name)
+	assert.Equal(t, newUser.Age, userUpdated.Age)
 }
