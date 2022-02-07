29611cf9ba06a047e2cb2b3c379e4e1f8787fa62
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-02-07 08:28:27 -0300

    test: handle update user
    test: handle update user

---
+++ pkg/api/handlers/handle_update_user_test.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/rafaelsanzio/go-core/pkg/errs"
+	"github.com/rafaelsanzio/go-core/pkg/repo"
+	"github.com/rafaelsanzio/go-core/pkg/user"
+)
+
+func TestHandleUpdateUser(t *testing.T) {
+	goodReq := httptest.NewRequest(http.MethodPut, fmt.Sprintf("/users/%s", "1"), nil)
+	goodReq = mux.SetURLVars(goodReq, map[string]string{})
+
+	body, err := json.Marshal(UserEntityPayload{
+		Name: "John Doe",
+		Age:  "38",
+	})
+	assert.Equal(t, nil, err)
+
+	goodReq.Body = ioutil.NopCloser(bytes.NewReader(body))
+
+	noBodyReq := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/users/%s", "1"), nil)
+	noBodyReq = mux.SetURLVars(noBodyReq, map[string]string{})
+
+	testCases := []struct {
+		Name               string
+		Request            *http.Request
+		ExpectedStatusCode int
+	}{
+		{
+			Name:               "Should return 200 if successful",
+			Request:            goodReq,
+			ExpectedStatusCode: 200,
+		}, {
+			Name:               "Should return bad request",
+			Request:            noBodyReq,
+			ExpectedStatusCode: 422,
+		},
+	}
+
+	repo.SetUserRepo(repo.MockUserRepo{
+		UpdateFunc: func(ctx context.Context, u user.User) (*user.User, errs.AppError) {
+			return &u, nil
+		},
+	})
+	defer repo.SetUserRepo(nil)
+
+	for _, tc := range testCases {
+		t.Log(tc.Name)
+
+		w := httptest.NewRecorder()
+
+		HandleUpdateUser(w, tc.Request)
+		assert.Equal(t, tc.ExpectedStatusCode, w.Code)
+	}
+}
