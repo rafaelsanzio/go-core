627e13f443de3dfa89dc2cbb037ecd50c82f376c
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-02-08 08:36:51 -0300

    test: handle delete user
    test: handle delete user

---
+++ pkg/api/handlers/handle_delete_user_test.go
@@ -0,0 +1,61 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/rafaelsanzio/go-core/pkg/errs"
+	"github.com/rafaelsanzio/go-core/pkg/repo"
+)
+
+func TestHandleDeleteUser(t *testing.T) {
+	repo.SetUserRepo(repo.MockUserRepo{
+		DeleteFunc: func(ctx context.Context, id string) errs.AppError {
+			if id == "1" {
+				return nil
+			}
+
+			return nil
+		},
+	})
+	defer repo.SetUserRepo(nil)
+
+	testCases := []struct {
+		Name               string
+		ID                 string
+		ExpectedStatusCode int
+	}{
+		{
+			Name:               "Success handle get user",
+			ID:                 "1",
+			ExpectedStatusCode: 204,
+		},
+		{
+			Name:               "Not Found handle get user",
+			ID:                 "",
+			ExpectedStatusCode: 404,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Log(tc.Name)
+
+		req, err := http.NewRequest(http.MethodDelete, "users/:id", nil)
+		req = mux.SetURLVars(req, map[string]string{"id": tc.ID})
+		assert.NoError(t, err)
+		res := httptest.NewRecorder()
+
+		HandleDeleteUser(res, req)
+
+		assert.Equal(t, tc.ExpectedStatusCode, res.Code)
+
+		if res.Code == 204 {
+			assert.NoError(t, err)
+		}
+	}
+}
