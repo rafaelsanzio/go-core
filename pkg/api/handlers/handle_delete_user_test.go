c381772735ac8791afcd4d5dac37998a524b6d04
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-04-25 08:58:28 -0300

    test: improving test cases and coverage
    test: improving test cases and coverage

--- pkg/api/handlers/handle_delete_user_test.go
+++ pkg/api/handlers/handle_delete_user_test.go
@@ -13,33 +13,48 @@ import (
 	"github.com/rafaelsanzio/go-core/pkg/repo"
 )
 
-func TestHandleDeleteUser(t *testing.T) {
-	repo.SetUserRepo(repo.MockUserRepo{
-		DeleteFunc: func(ctx context.Context, id string) errs.AppError {
-			return nil
-		},
-	})
-	defer repo.SetUserRepo(nil)
+func mockDeleteUserFunc(ctx context.Context, id string) errs.AppError {
+	return nil
+}
 
+func mockDeleteUserThrowFunc(ctx context.Context, id string) errs.AppError {
+	return errs.ErrMongoDeleteOne
+}
+
+func TestHandleDeleteUser(t *testing.T) {
 	testCases := []struct {
-		Name               string
-		ID                 string
-		ExpectedStatusCode int
+		Name                 string
+		ID                   string
+		HandleDeleteFunction func(ctx context.Context, id string) errs.AppError
+		ExpectedStatusCode   int
 	}{
 		{
-			Name:               "Success handle delete user",
-			ID:                 "1",
-			ExpectedStatusCode: 204,
+			Name:                 "Success handle delete user",
+			ID:                   "1",
+			HandleDeleteFunction: mockDeleteUserFunc,
+			ExpectedStatusCode:   204,
 		},
 		{
-			Name:               "Not Found handle delete user",
-			ID:                 "",
-			ExpectedStatusCode: 404,
+			Name:                 "Not Found handle delete user",
+			ID:                   "",
+			HandleDeleteFunction: mockDeleteUserFunc,
+			ExpectedStatusCode:   404,
+		},
+		{
+			Name:                 "Throwing error on function",
+			ID:                   "1",
+			HandleDeleteFunction: mockDeleteUserThrowFunc,
+			ExpectedStatusCode:   500,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Log(tc.Name)
+
+		repo.SetUserRepo(repo.MockUserRepo{
+			DeleteFunc: tc.HandleDeleteFunction,
+		})
+		defer repo.SetUserRepo(nil)
 
 		req, err := http.NewRequest(http.MethodDelete, "users/:id", nil)
 		req = mux.SetURLVars(req, map[string]string{"id": tc.ID})
