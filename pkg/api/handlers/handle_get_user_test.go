21d617670b35f0f52bc6832a0671e59e4af18590
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-04-29 10:21:24 -0300

    test: mocking marshal function and add test case
    test: mocking marshal function and add test case

--- pkg/api/handlers/handle_get_user_test.go
+++ pkg/api/handlers/handle_get_user_test.go
@@ -10,11 +10,20 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/rafaelsanzio/go-core/pkg/applog"
 	"github.com/rafaelsanzio/go-core/pkg/errs"
 	"github.com/rafaelsanzio/go-core/pkg/model"
 	"github.com/rafaelsanzio/go-core/pkg/repo"
 	"github.com/rafaelsanzio/go-core/pkg/user"
 )
+
+func fakeMarshal(v interface{}) ([]byte, error) {
+	return []byte{}, errs.ErrMarshalingJson.Throwf(applog.Log, errs.ErrFmt)
+}
+
+func restoreMarshal(replace func(v interface{}) ([]byte, error)) {
+	jsonMarshal = replace
+}
 
 func mockGetUserFunc(ctx context.Context, id string) (*user.User, errs.AppError) {
 	if id == "1" {
@@ -33,24 +42,35 @@ func TestHandleGetUser(t *testing.T) {
 		Name                  string
 		ID                    string
 		HandleGetUserFunction func(ctx context.Context, id string) (*user.User, errs.AppError)
+		MarshalFunction       func(v interface{}) ([]byte, error)
 		ExpectedStatusCode    int
 	}{
 		{
 			Name:                  "Success handle get user",
 			ID:                    "1",
 			HandleGetUserFunction: mockGetUserFunc,
+			MarshalFunction:       jsonMarshal,
 			ExpectedStatusCode:    200,
 		},
 		{
 			Name:                  "Not Found handle get user",
 			ID:                    "",
 			HandleGetUserFunction: mockGetUserFunc,
+			MarshalFunction:       jsonMarshal,
 			ExpectedStatusCode:    404,
 		},
 		{
 			Name:                  "Error getting repo user",
 			ID:                    "1",
 			HandleGetUserFunction: mockGetUserThrowFunc,
+			MarshalFunction:       jsonMarshal,
+			ExpectedStatusCode:    500,
+		},
+		{
+			Name:                  "Error getting marshal err",
+			ID:                    "1",
+			HandleGetUserFunction: mockGetUserFunc,
+			MarshalFunction:       fakeMarshal,
 			ExpectedStatusCode:    500,
 		},
 	}
@@ -62,6 +82,9 @@ func TestHandleGetUser(t *testing.T) {
 			GetFunc: tc.HandleGetUserFunction,
 		})
 		defer repo.SetUserRepo(nil)
+
+		jsonMarshal = tc.MarshalFunction
+		defer restoreMarshal(jsonMarshal)
 
 		req, err := http.NewRequest(http.MethodGet, "users/:id", nil)
 		req = mux.SetURLVars(req, map[string]string{"id": tc.ID})
