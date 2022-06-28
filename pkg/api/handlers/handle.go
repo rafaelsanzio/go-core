d0c3cebe2507d952b0764f26fb3abefa5ac654bb
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-06-28 09:05:08 -0300

    fix: change lint error
    fix: change lint error

--- pkg/api/handlers/handle.go
+++ pkg/api/handlers/handle.go
@@ -12,7 +12,7 @@ func HandleAdapter(hf http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-		if limiter.Allow() == false {
+		if !limiter.Allow() {
 			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
 			return
 		}
