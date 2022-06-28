50e5b7c55196aef9aa63bba19ba51e1cb6fd2339
Rafael Sanzio <rafaelsanzio27@gmail.com>
2022-06-28 08:30:00 -0300

    feat: implement rate limit to api calls
    feat: implement rate limit to api calls

--- pkg/api/handlers/handle.go
+++ pkg/api/handlers/handle.go
@@ -1,10 +1,22 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"golang.org/x/time/rate"
+)
+
+var limiter = rate.NewLimiter(1, 2)
 
 func HandleAdapter(hf http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+		if limiter.Allow() == false {
+			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
+			return
+		}
+
 		hf(w, r)
 	}
 }
